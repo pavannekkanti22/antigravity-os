7bf6760dc6e549e8825c5b91dde9769309b55873
pavannekkanti22 <kishorenekkanti12@gmail.com>
2026-06-22 12:07:21 +0530

    Remove backend Java files from frontend repo

--- src/main/java/com/antigravity/backend/repository/SystemSettingsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.antigravity.backend.repository;
-
-import com.antigravity.backend.entity.SystemSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SystemSettingsRepository
-        extends JpaRepository<SystemSettings, Long> {
-}
